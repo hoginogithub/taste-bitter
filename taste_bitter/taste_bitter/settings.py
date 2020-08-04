9b825a396fd5c1bced516a96066a63a105713502
hoginogithub <hogino@h7.dion.ne.jp>
2020-08-04 21:27:48 +0900

    debug message on

--- taste_bitter/taste_bitter/settings.py
+++ taste_bitter/taste_bitter/settings.py
@@ -144,7 +144,6 @@ if not DEBUG:
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
-"""
 # ロギング設定
 LOGGING = {
     'version': 1,
@@ -185,4 +184,3 @@ LOGGING = {
         },
     }
 }
-"""
