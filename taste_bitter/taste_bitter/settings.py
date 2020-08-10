9883dcdbcee646d66864dc5cdd7ea3b403f3b1b5
hoginogithub <hogino@h7.dion.ne.jp>
2020-08-10 21:05:55 +0900

    settings.py delete STATICFILES_DIRS, STATICFILES_STPRAGE

--- taste_bitter/taste_bitter/settings.py
+++ taste_bitter/taste_bitter/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from .local_settings import *
